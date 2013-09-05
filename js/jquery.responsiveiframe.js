87f9b96120abc2c9796b6f36c3619ec9b03e8376
Michael Seid <mbseid@gmail.com>
2013-09-05 14:34:50 -0400

    Fixed origin, to the updated responsiveiframe.

--- js/jquery.responsiveiframe.js
+++ js/jquery.responsiveiframe.js
@@ -49,7 +49,7 @@ if (typeof jQuery !== 'undefined') {
 
                 if (settings.xdomain !== '*') {
                     var regex = new RegExp(settings.xdomain + '$');
-                    if(e.orgin == "null"){
+                    if(e.origin == "null"){
                         throw new Error("messageHandler( elem, e): There is no origin.  You are viewing the page from your file system.  Please run through a web server.");
                     }
                     if(e.origin.match(regex)){
