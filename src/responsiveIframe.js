baafa039a21adf4b0084a39317cf8f6f9e67568f
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-04-10 20:00:16 -0700

    Final internal refactor of JS; also adds comments and inline documentation.

--- src/responsiveIframe.js
+++ src/responsiveIframe.js
@@ -1,200 +1,225 @@
 /*
 * This is a responsive iFrame library.
 * Check out the docs at http://foo or the readme at README.md for usage.
-
 */
 
-window.responsiveChild = function(config){
-    var parentWidth;
-    var id;
-    var settings = {
-        renderCallback: null,
-        id:null,
-        xdomain: '*',
-        polling: 0
-    };
-    for (var key in config) { settings[key] = config[key]; }
-
-    /*
-    * Extract a querystring parameter from the URL.
-    */
-    var getParameterByName = function(name) {
-        name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
-
-        var regex = new RegExp("[\\?&]" + name + '=([^&#]*)');
-        var results = regex.exec(location.search);;
-
-        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, " "));
-    };
-
-    /*
-    * Verify that the message came from a trustworthy domaine
-    */
-    var isSafeMessage = function(e) {
-        if (settings.xdomain !== '*') {
-            var regex = new RegExp(settings.xdomain + '$');
-
-            if (!e.origin.match(regex)) {
-                // Not the origin we're listening for
-                return;
-            }
-        }
-
-        return true;
-    };
-
-    /*
-    * Process a new message from parent frame.
-    */
-    var processMessage = function(e) {
-        if (!isSafeMessage(e)) {
-            return;
-        }
-
-        // Parent sent width
-        var match = e.data.match(/^responsiveparent (\S+) (\d+)$/);
-
-        if (!match || match.length !== 3) {
-            // Not the message we're listening for
-            return;
-        }
-
-        id = match[1];
-        var width = parseInt(match[2]);
-
-        if (width != parentWidth) {
-            parentWidth = width;
-
-            if (settings.renderCallback) {
-                settings.renderCallback(width);
-            }
-
-            window.responsiveChild.sendHeightToParent();
-        }
-    };
-
-    /*
-    * Transmit the current iframe height to the parent.
-    */
-    window.responsiveChild.sendHeightToParent = function() {
-        var height = document.getElementsByTagName('body')[0].offsetHeight.toString();
-        window.top.postMessage('responsivechild ' + id + ' '+ height, '*');
-    };
-
-    window.addEventListener('message', processMessage, false);
-
-    id = getParameterByName('childId');
-
-    // Initial width is sent as querystring parameter
-    var width = parseInt(getParameterByName('initialWidth'));
-
-    if (settings.renderCallback) {
-        settings.renderCallback(width);
-    }
-
-    window.responsiveChild.sendHeightToParent();
-
-    if (settings.polling) {
-        window.setInterval(window.responsiveChild.sendHeightToParent, settings.polling);
-    }
-
-}
-
 window.responsiveParent = function(config) {
+    /*
+    * A global function for setting up a responsive parent.
+    * Use the config object to override the default settings.
+    */
+
+    // Grab all of the divs which should contain a responsive iframe.
+    // This becomes a list of elements.
     var elements = document.querySelectorAll('div[data-iframe-target]');
+
+    // Default settings.
     var settings = { xdomain: '*' };
 
+    // Add any overrides to settings coming from config.
     for (var key in config) { settings[key] = config[key]; }
 
     var constructIframe = function(el) {
-        // Calculate its width.
+        /*
+        * A function which handles constructing an iframe inside a given element.
+        */
+
+        // Calculate the width of this element.
         var width = el.offsetWidth.toString();
 
+        // Create an iframe element attached to the document.
         var node = document.createElement("iframe");
 
+        // Get the URL to the child page from the element's data-iframe-target attribute.
         var url = el.getAttribute('data-iframe-target');
+
+        // Get the ID of the element to enable messages to this specific iframe child.
         var id = el.getAttribute('id');
 
+        // If the URL contains querystring bits, use them.
+        // Otherwise, just create a set of valid params.
         if (url.indexOf('?') < 0) {
             url += '?';
         } else {
             url += '&';
         }
 
-        // Send the initial width as a querystring parameter.
+        // Append the initial width as a querystring parameter.
         node.src = url + 'initialWidth=' + width + '&childId=' + id;
 
-        // Super not-dry way to set attrs on our iFrame.
+        // Set some attributes to this proto-iframe.
         node.setAttribute('width', '100%');
         node.setAttribute('scrolling', 'no');
         node.setAttribute('marginheight', '0');
         node.setAttribute('frameborder', '0');
+
+        // Append the iframe to our element.
         el.appendChild(node);
 
-        // Do our magic on screen resize.
-        window.addEventListener('resize', function(e) {
-            sendWidthToChild(el);
-        });
+        // Add an event listener that will handle redrawing the child on resize.
+        window.addEventListener('resize', function(e) { sendWidthToChild(el); });
     }
 
-    /*
-    * Verify that the message came from a trustworthy domain.
-    */
-    var isSafeMessage = function(e) {
-        if (settings.xdomain !== '*') {
-            var regex = new RegExp(settings.xdomain + '$');
+    var processChildMessage = function(e) {
+        /*
+        * Process a new message from the child.
+        * Used to set the height on our iframe.
+        */
+        if (!window.isSafeMessage(e, settings)) { return; }
 
-            if (!e.origin.match(regex)) {
-                // Not the origin we're listening for
-                return;
-            }
-        }
-
-        return true;
-    }
-
-    /*
-    * Process a new message from a child iframe.
-    */
-    var processMessage = function(e) {
-        if (!isSafeMessage(e)) {
-            return;
-        }
-
-        // Child sent height
+        // Grab the message from the child and parse it.
         var match = e.data.match(/^responsivechild (\S+) (\d+)$/);
 
-        console.log(match);
+        // If there's no match or too many matches in the message, punt.
+        if (!match || match.length !== 3) { return false; }
 
-        if (!match || match.length !== 3) {
-            return false;
-        }
-
+        // Get the ID from the message.
         var childId = match[1];
+
+        // Get the child's height from the message.
         var height = parseInt(match[2]);
 
+        // Get a list of all of the responsive iframe containers on the page.
         elements = document.querySelectorAll('div[data-iframe-target]');
 
+        // Loop over the containers.
         for (var i=0; i<elements.length; i++){
+
+            // Get this container's element.
             el = elements[i];
+
+            // See if this container is the one from the message.
             if (el.getAttribute('id') == childId) {
+
+                // If so, update the height.
                 el.getElementsByTagName('iframe')[0].setAttribute('height', height + 'px');
             }
         }
     }
 
-    /*
-    * Transmit the current iframe width to the child.
-    */
     function sendWidthToChild(el) {
+        /*
+        * Transmit the current iframe width to the child.
+        */
+
+        // Get the width of the element.
         var width = el.offsetWidth.toString();
+
+        // Pass the width out to the child so it can compute the height.
         el.getElementsByTagName('iframe')[0].contentWindow.postMessage('responsiveparent ' + el.getAttribute('id') + ' ' + width, '*');
     }
 
-    window.addEventListener('message', processMessage, false);
+    // Add a listener for processing messages from the child.
+    window.addEventListener('message', processChildMessage, false);
 
+    // Loop over the responsive iframe container elements and construct iframes in them.
     for (var i=0; i<elements.length; i++){
         constructIframe(elements[i]);
+    }
+
+};
+
+window.isSafeMessage = function(e, settings) {
+    /*
+    * Check the message to make sure it comes from an acceptable xdomain.
+    * Defaults to '*' but can be overriden in config.
+    */
+    if (settings.xdomain !== '*') {
+        // If origin doesn't match our xdomain, return.
+        if (!e.origin.match(new RegExp(settings.xdomain + '$'))) { return; }
+    }
+
+    return true;
+}
+
+window.responsiveChild = function(config) {
+    /*
+    * A global function for setting up a responsive child.
+    * Use the config object to override the default settings.
+    */
+    var parentWidth;
+    var id;
+
+    var settings = {
+        renderCallback: null,
+        xdomain: '*',
+        polling: 0
+    };
+    for (var key in config) { settings[key] = config[key]; }
+
+    var getParameterByName = function(name) {
+        /*
+        * Generic function for parsing URL params.
+        * Via http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+        */
+        var regex = new RegExp("[\\?&]" + name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]') + '=([^&#]*)');
+        var results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, " "));
+    };
+
+    var processParentMessage = function(e) {
+        /*
+        * Process a new message from parent frame.
+        */
+
+        // First, punt if this isn't from an acceptable xdomain.
+        if (!window.isSafeMessage(e, settings)) { return; }
+
+        // Get the message from the parent.
+        var match = e.data.match(/^responsiveparent (\S+) (\d+)$/);
+
+        // If there's no match or it's a bad format, punt.
+        if (!match || match.length !== 3) { return; }
+
+        // Get the ID out of the message.
+        id = match[1];
+
+        // Get the width out of the message.
+        var width = parseInt(match[2]);
+
+        // Change the width if it's different.
+        if (width != parentWidth) {
+            parentWidth = width;
+
+            // Call the callback function if it exists.
+            if (settings.renderCallback) { settings.renderCallback(width); }
+
+            // Send the height back to the parent.
+            window.responsiveChild.sendHeightToParent();
+        }
+    };
+
+    window.responsiveChild.sendHeightToParent = function() {
+        /*
+        * Transmit the current iframe height to the parent.
+        * Make this callable from external scripts in case they update the body out of sequence.
+        */
+
+        // Get the child's height.
+        var height = document.getElementsByTagName('body')[0].offsetHeight.toString();
+
+        // Send the height to the parent.
+        window.top.postMessage('responsivechild ' + id + ' '+ height, '*');
+    };
+
+    // Set up a listener to handle any incoming messages.
+    window.addEventListener('message', processParentMessage, false);
+
+    // Identify what ID the parent knows this child as.
+    id = getParameterByName('childId');
+
+    // Get the initial width from a URL parameter.
+    var width = parseInt(getParameterByName('initialWidth'));
+
+    // If there's a callback function, call it.
+    if (settings.renderCallback) { settings.renderCallback(width); }
+
+    // Send the initial height to the parent.
+    window.responsiveChild.sendHeightToParent();
+
+    // If we're configured to poll, create a setInterval to handle that.
+    if (settings.polling) {
+        window.setInterval(window.responsiveChild.sendHeightToParent, settings.polling);
     }
 
 };
