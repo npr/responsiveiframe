b9676531cb6977aa5e48c863754c2caa144c6516
Christopher Groskopf <staringmonkey@gmail.com>
2014-07-09 10:03:53 -0700

    Comments + cleanup.

--- src/pym.js
+++ src/pym.js
@@ -10,7 +10,7 @@
         window.pym = factory.call(this);
     }
 })(function() {
-    var DELIMITER = 'PYM';
+    var MESSAGE_DELIMITER = 'PYM';
 
     var lib = {};
 
@@ -28,8 +28,22 @@
     }
 
     var _makeMessage = function(id, messageType, message) {
-        // use string building because JSON is not supported in all browsers.
-        return 'pym' + DELIMITER + id + DELIMITER + messageType + DELIMITER + message;
+        /*
+         * We use string-building here because JSON message passing is
+         * not supported in all browsers.
+         */
+        var bits = ['pym', id, messageType, message];
+
+        return bits.join(MESSAGE_DELIMITER);
+    }
+
+    var _makeMessageRegex = function(id) {
+        /*
+         * Construct a regex to match passed messages.
+         */
+        var bits = ['pym', id, '(\\S+)', '(.+)'];
+
+        return new RegExp('^' + bits.join(MESSAGE_DELIMITER) + '$');
     }
 
     var _autoInit = function() {
@@ -82,8 +96,7 @@
             xdomain: '*'
         };
 
-        this.messageRegex = new RegExp('^pym' + DELIMITER + this.id + DELIMITER + '(\\S+)' + DELIMITER + '(.+)$');
-
+        this.messageRegex = _makeMessageRegex(this.id); 
         this.messageHandlers = {};
 
         this.constructIframe = function() {
@@ -157,11 +170,17 @@
             }
         };
 
-        this.sendMessage = function(messageType, message) {
+        this.sendMessageToChild = function(messageType, message) {
+            /*
+             * Send a message to the the child.
+             */
             this.el.getElementsByTagName('iframe')[0].contentWindow.postMessage(_makeMessage(this.id, messageType, message), '*');
         };
 
         this.on = function(messageType, callback) {
+            /*
+             * Bind a callback to a message from the child.
+             */
             if (!(messageType in this.messageHandlers)) {
                 this.messageHandlers[messageType] = [];
             }
@@ -170,10 +189,11 @@
         };
 
         this._onHeightMessage = function(data) {
-            // Get the child's height from the message.
+            /*
+             * Handle parent message from child.
+             */
             var height = parseInt(data);
 
-            // Update the height.
             this.el.getElementsByTagName('iframe')[0].setAttribute('height', height + 'px');
         };
 
@@ -181,12 +201,9 @@
             /*
             * Transmit the current iframe width to the child.
             */
-
-            // Get the width of the element.
             var width = this.el.offsetWidth.toString();
 
-            // Pass the width out to the child so it can compute the height.
-            this.sendMessage('width', width);
+            this.sendMessageToChild('width', width);
         };
 
         // Add any overrides to settings coming from config.
@@ -194,6 +211,7 @@
             this.settings[key] = config[key];
         }
 
+        // Add height event callback 
         this.on('height', this._onHeightMessage);
 
         // Add a listener for processing messages from the child.
@@ -232,6 +250,7 @@
             */
             var regex = new RegExp("[\\?&]" + name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]') + '=([^&#]*)');
             var results = regex.exec(location.search);
+
             return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, " "));
         };
 
@@ -245,7 +264,7 @@
             var height = document.getElementsByTagName('body')[0].offsetHeight.toString();
 
             // Send the height to the parent.
-            this.sendMessage('height', height);
+            this.sendMessageToParent('height', height);
         };
 
         this.processParentMessage = function(e) {
@@ -270,12 +289,18 @@
             }
         };
 
-        this.sendMessage = function(messageType, message) {
+        this.sendMessageToParent = function(messageType, message) {
+            /*
+             * Send a message to the parent.
+             */
             window.top.postMessage(_makeMessage(this.id, messageType, message), '*');
         };
 
 
         this.on = function(messageType, callback) {
+            /*
+             * Bind to a callback to a message from the parent.
+             */
             if (!(messageType in this.messageHandlers)) {
                 this.messageHandlers[messageType] = [];
             }
@@ -284,6 +309,9 @@
         };
 
         this._onWidthMessage = function(data) {
+            /*
+             * Handle width message from the child.
+             */
             var width = parseInt(data);
 
             // Change the width if it's different.
@@ -302,13 +330,12 @@
 
         // Identify what ID the parent knows this child as.
         this.id = this.getParameterByName('childId');
-        this.messageRegex = new RegExp('^pym' + DELIMITER + this.id + DELIMITER + '(\\S+)' + DELIMITER + '(.+)$');
-
-        console.log(this.messageRegex);
+        this.messageRegex = new RegExp('^pym' + MESSAGE_DELIMITER + this.id + MESSAGE_DELIMITER + '(\\S+)' + MESSAGE_DELIMITER + '(.+)$');
 
         // Get the initial width from a URL parameter.
         var width = parseInt(this.getParameterByName('initialWidth'));
 
+        // Bind the width message handler
         this.on('width', this._onWidthMessage);
 
         // Initialize settings with overrides.
